53ed5798d7e0468cc009e5fc048442c4d9eedc20
Ben Werling <ben.werling@googlemail.com>
2020-09-29 11:42:19 +0200

    Adds UserPage
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- src/models/IUser.tsx
+++ src/models/IUser.tsx
@@ -1,8 +1,18 @@
 interface IUser {
-  id: string;
-  login: string;
   avatar_url: string;
+  bio: string;
+  blog: string;
+  company: string;
+  followers: number;
+  following: number;
+  hireable: boolean;
   html_url: string;
+  id: string;
+  location: string;
+  login: string;
+  name: string;
+  public_gists: number;
+  public_repos: number;
 }
 
 export default IUser;
