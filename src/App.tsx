b7b1cc58c823f72466c41f308306204e51903ec1
Ben Werling <ben.werling@googlemail.com>
2020-10-01 20:44:49 +0200

    Adds additional pages
    Signed-off-by: Ben Werling <ben.werling@googlemail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
-import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
-import Users from "./components/users/Users";
+import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
+import Navbar from "./components/layout/Navbar";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
 
 import AlertState from "./context/alert/AlertState";
@@ -23,22 +23,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
-                <Route
-                  exact
-                  path="/user/:login"
-                  render={(props) => <User {...props} />}
-                />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
